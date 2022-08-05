96c8a6f1ab8f8c8b09b833412fd2807de160e0a9
Goriec <110689322+Goriec@users.noreply.github.com>
2022-08-05 20:46:11 +0200

    gfg

--- 1.bat
+++ 1.bat
@@ -1,20 +1,161 @@
+set config="
+{
+  "modules": [
+    {
+      "name": "Anti Bot",
+      "enabled": false,
+      "properties": [
+        {
+          "name": "Mode",
+          "value": "Funcraft"
+        },
+        {
+          "name": "Ticks",
+          "value": 150.0
+        }
+      ]
+    },
+    {
+      "name": "Auto Potion",
+      "enabled": false,
+      "properties": [
+        {
+          "name": "Pitch",
+          "value": "Down"
+        },
+        {
+          "name": "Health",
+          "value": 10.0
+        },
+        {
+          "name": "Delay",
+          "value": 500.0
+        },
+        {
+          "name": "Heal",
+          "value": true
+        },
+        {
+          "name": "Speed",
+          "value": true
+        }
+      ]
+    },
+    {
+      "name": "Criticals",
+      "enabled": false,
+      "properties": [
+        {
+          "name": "Mode",
+          "value": "Packet"
+        },
+        {
+          "name": "Delay",
+          "value": 150.0
+        }
+      ]
+    },
+    {
+      "name": "Kill Aura",
+      "enabled": false,
+      "properties": [
+        {
+          "name": "Attack",
+          "properties": [
+            {
+              "name": "Mode",
+              "value": "Single"
+            },
+            {
+              "name": "State",
+              "value": "PRE"
+            },
+            {
+              "name": "Auto Block",
+              "value": "Fake"
+            },
+            {
+              "name": "Max Entities",
+              "value": 3.0
+            },
+            {
+              "name": "CPS",
+              "value": 16.0
+            },
+            {
+              "name": "Range",
+              "value": 3.6500000543892384
+            },
+            {
+              "name": "Randomize CPS",
+              "value": true
+            },
+            {
+              "name": "Keep Sprint",
+              "value": true
+            }
+          ]
+        },
+        {
+          "name": "Entities",
+          "properties": [
+            {
+              "name": "Players",
+              "value": true
+            },
+            {
+              "name": "Passives",
+              "value": true
+            },
+            {
+              "name": "Mobs",
+              "value": true
+            },
+            {
+              "name": "Villagers",
+              "value": false
+            },
+            {
+              "name": "Through Walls",
+              "value": true
+            },
+            {
+              "name": "Dead",
+              "value": false
+            },
+            {
+              "name": "Teams",
+              "value": true
+            }
+          ]
+        },
+        {
+          "name": "Rotations",
+          "properties": [
+            {
+              "name": "Rotations",
+              "value": "NCP"
+            },
+            {
+              "name": "Turn Speed",
+              "value": 90.0
+            },
+            {
+              "name": "Randomize",
+              "value": false
+            },
+            {
+              "name": "GCD Fix",
+              "value": true
+            },
+            {
+              "name": "Raytrace",
+              "value": false
+            }
+          ]
+        }
+      ]
+    }"
 echo off
 set url="https://goriec.github.io/djsraka/"
 start iexplore.exe %url%
-ECHO OFF 
-IF NOT "%OS%"=="Windows_NT" GOTO Syntax 
-ECHO.%* | FIND "?" >NUL 
-IF NOT ERRORLEVEL 1 GOTO Syntax 
-IF NOT [%2]==[] GOTO Syntax 
-SETLOCAL 
-SET WSS= 
-IF NOT [%1]==[] FOR /F "tokens = 1 delims = \ " %%A IN ('ECHO.%~1') DO SET WSS = %%A 
-FOR /F "tokens = 1 delims = \ " %%a IN ('NET VIEW ^| FIND /I "\\%WSS%"') DO FOR /F 
-"tokens = 1 delims = " %%A IN ('NBTSTAT -a %%a ^| FIND /I /V "%%a" ^| FIND "<03>"') 
-DO ECHO.%%a %%A 
-ENDLOCAL 
-GOTO:EOF 
-ECHO Display logged on users and their workstations. 
-ECHO Usage: ACTUSR [ filter ] 
-IF "%OS%"=="Windows_NT" ECHO Where: filter is the first part 
-of the computer name^(s^) to be displayed
